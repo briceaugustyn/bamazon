82ecbcf432613c7e39f90fbe00f6c3f03d24c024
Brice Augustyn <baugustyn1000@gmail.com>
2018-04-21 10:20:18 -0700

    prompt squence

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,39 +11,50 @@ var connection = mysql.createConnection({
 });
 
 
-// Start the prompt 
+//console.log('HELLO WELCOME TO OUR BAMAZON DATABASE');
+//console.log('HERE IS OUR PRODUCTS');
 
+
+//Start the prompt 
 prompt.start();
-prompt.get(['would you like to "POST AN ITEM" or "BID ON AN ITEM?'], function (err, result) {
+
+
+prompt.get(['would you like to see our database ?'], function (err, result) {
     console.log('Command-line input received:');
     console.log('  answwer: ' + result[0]);
+    Display_database()
     //p1.test1()
     //p1.test()
-
 
 });
 
 
+// prompt.get(['would you like to "POST AN ITEM" or "BID ON AN ITEM?'], function (err, result) {
+//     console.log('Command-line input received:');
+//     console.log('  answwer: ' + result[0]);
+   
+//     //p1.test1()
+//     //p1.test()
+
+// });
 
 
-connection.connect((error) => {
-    if (error) throw error
-    console.log('connected as ', connection.threadId)
 
-    // connection.query(insert1, (error, response) => {
-    //     if (error) throw error
 
-    // }
+function Display_database(){
 
-    // )
-
-    connection.query("select * from products ", (error, response) => {
-
+    connection.connect((error) => {
         if (error) throw error
-        console.log(response[1].product_name)
-    })
-    connection.end()
+       
+        connection.query("select item_id,product_name,price from products ", (error, result, fields) => {
+            if (error) throw error
+            console.log(result);
+           // console.log(response[1].product_name)
+        })
+        connection.end()
+    }
+    
+    
+    )
+
 }
-
-
-)
